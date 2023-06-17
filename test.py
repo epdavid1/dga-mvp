a72f223f1c7dbe8bd738bea740d237dad2df45e0
Edson <110080859+epdavid1@users.noreply.github.com>
2023-06-18 01:35:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ import numpy as np
 tst_noise = load_learner('tst_noise')
 tst_test = pd.read_csv('test.csv')
 
-start = time.time()
-tst_noise.get_X_preds(tst_test.iloc[0].to_numpy().reshape(1,1,10001))[2].astype(int)
-print(time.time()-start)
+iter = np.zeros(400)
+for i in range(0,400):
+	start = time.time()
+	tst_noise.get_X_preds(tst_test.iloc[0].to_numpy().reshape(1,1,10001))	[2].astype(int)
+	iter[i] = time.time()-start
+print('Average detection time for 400 trials = '+str(int(np.round(np.mean(iter)*1000)))+' ms')
