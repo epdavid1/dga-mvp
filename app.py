b2e27f40341488657a1e4333b6af8f709880df54
Edson <110080859+epdavid1@users.noreply.github.com>
2024-01-23 09:17:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def fault_score(df, address, lat, lng):
     st.markdown(style.style_metric(), unsafe_allow_html=True)
 
     with container:
-        st.markdown("<h2>Transformer Dissolved Gas Monitoring</h2>", unsafe_allow_html=True)        
+        st.markdown("<h2>Transformer Monitoring</h2>", unsafe_allow_html=True)        
         st.caption(f"As of {get_current_time()}")
     
     
@@ -225,7 +225,7 @@ def main():
 
         
     with chart_col:
-        st.subheader("Power Transformer Map")
+        st.subheader("Transformer Map")
         st.caption("Select a pin to select a substation: ")
         st.markdown(" ")
         st.markdown(" ")      
